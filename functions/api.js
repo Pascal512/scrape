c309af281ca1d1a90748e52b81101f40b8b9ce0b
Pascal <112500075+Pascal512@users.noreply.github.com>
2024-10-27 16:13:11 +0100

    Test modification route

--- functions/api.js
+++ functions/api.js
@@ -66,5 +66,6 @@ app.get('/scrape/:type/:url(*)', async (req, res) => {
     console.log(`API de scraping en cours d'exécution sur http://localhost:${port}`);
 });*/
 
-app.use('/.netlify/functions/api', router);
+//app.use('/.netlify/functions/api', router);
+app.use('/', router);
 module.exports.handler = serverless(app);
